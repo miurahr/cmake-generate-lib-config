af674e28946986b678b88febc41db4e21a976cf1
Hiroshi Miura <miurahr@linux.com>
2018-11-03 20:44:39 +0900

    generalize module
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- Modules/GenerateConfig.cmake
+++ Modules/GenerateConfig.cmake
@@ -1,9 +1,7 @@
-
-
 include(SelectImportedConfig)
-include(SplitLibraryToCFlag)
+include(SplitLibraryToCFlags)
 
-function(generate_config _target _output)
+function(generate_config _target _link _template _output)
     # CONFIG_PREFIX, CONFIG_CFLAGS, CONFIG_DATADIR, CONFIG_LIBS
     if(NOT DEFINED CMAKE_INSTALL_PREFIX)
         set(CONFIG_PREFIX "/usr/local") # default
@@ -11,13 +9,17 @@ function(generate_config _target _output)
         set(CONFIG_PREFIX ${CMAKE_INSTALL_PREFIX})
     endif()
     set(CONFIG_CFLAGS "-I${CONFIG_PREFIX}/include")
-    set(CONFIG_DATADIR "${CONFIG_PREFIX}/share/${_target}/${${_target}_VERSION_MAJOR}.${${_target}_VERSION_MINOR}")
-    set(CONFIG_LIBS "-L${CONFIG_PREFIX}/${CMAKE_INSTALL_LIBDIR} -l${_target}")
-        set(CONFIG_VERSION ${${_target}_VERSION})
+    set(CONFIG_DATADIR "${CONFIG_PREFIX}/share/${_target}")
+    if(CONFIG_PREFIX STREQUAL "/usr")
+        set(CONFIG_LIBS "${CMAKE_LINK_LIBRARY_FLAG}${_target}")
+    else()
+        set(CONFIG_LIBS "${CMAKE_LIBRARY_PATH_FLAG}${CONFIG_PREFIX}/lib ${CMAKE_LINK_LIBRARY_FLAG}${_target}")
+    endif()
+    set(CONFIG_VERSION ${${_target}_VERSION})
 
     # CONFIG_DEP_LIBS
     set(_DEP_LIBS "")
-    get_property(_LIBS TARGET interface_library PROPERTY INTERFACE_LINK_LIBRARIES)
+    get_property(_LIBS TARGET "${_link}" PROPERTY INTERFACE_LINK_LIBRARIES)
     list(REMOVE_DUPLICATES _LIBS)
     foreach(_lib IN LISTS _LIBS)
         if(NOT TARGET ${_lib})
@@ -38,17 +40,20 @@ function(generate_config _target _output)
                 endif()
             elseif(_type STREQUAL "UNKNOWN_LIBRARY")
                 # IMPORTED UNKOWN
-                get_property(_res TARGET ${_lib} PROPERTY IMPORTED_CONFIGURATIONS_RELEASE SET)
+                get_property(_res TARGET ${_lib} PROPERTY IMPORTED_CONFIGURATIONS SET)
                 if(_res) # use imported target with configurations
-                    select_imported_config(example _conf)
+                    select_imported_config(${_target} _conf)
+                    if(NOT _conf)
+                        set(_conf RELEASE)
+                    endif()
                     string(TOUPPER ${_conf} _BT)
                     get_property(_imp TARGET ${_lib} PROPERTY IMPORTED_LOCATION_${_BT})
                     split_library_to_cflags("${_imp}" _res)
                     list(APPEND _DEP_LIBS "${_res}")
                 else() # just use default location
                     get_property(_imp TARGET ${_lib} PROPERTY IMPORTED_LOCATION)
-                    split_library_to_cflags("${_imp}" _val)
-                    list(APPEND _DEP_LIBS "${_val}")
+                    split_library_to_cflags("${_imp}" _res)
+                    list(APPEND _DEP_LIBS "${_res}")
                 endif()
             endif()
         endif()
@@ -56,10 +61,7 @@ function(generate_config _target _output)
     string(REPLACE ";" " " CONFIG_DEP_LIBS "${_DEP_LIBS}")
 
     # Generate lib-config
-    file(READ ${CMAKE_CURRENT_LIST_DIR}/lib-config.in LIB_CONFIG_CONTENT)
+    file(READ ${_template} LIB_CONFIG_CONTENT)
     string(CONFIGURE "${LIB_CONFIG_CONTENT}" LIB_CONFIG_CONTENT @ONLY)
     file(GENERATE OUTPUT ${_output} CONTENT "${LIB_CONFIG_CONTENT}")
-    install(PROGRAMS ${_output}
-            DESTINATION bin
-            PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
 endfunction()
--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Distill lib dependencies and produce cflags
+# Generate example-config shell script
 
 There are sometimes produce `example-config` command that works like `pkg-config` 
 to provide a build configurations, when you develop library and distribute.
 
 This is a function to produce `example-config`.
+
+## How to use?
+
+1. Place cmake scripts under `Modules` folder in your project and add search path
+in your `CMakeLists.txt` by setting `CMAKE_MODULE_PATH` variable.
+
+2. Place `lib-config.in` template file in your project.
+
+3. Include script using `include(GenearateConfig)` in your `CMakeLists.txt`
+
+4. Set variable `CONFIG_DATADIR` to fit your project.
+
+5. Call function `generate_config(<target_name> <output_filename>)`.
+
+## Limitations
+
+This module assumes you use a specific way to link external library with your project.
+It is happened with CMake features and limitations.
+
+* You should defines INTERFACE library and link all external libraries with it
+  if you have several subfolders in your project.
 
